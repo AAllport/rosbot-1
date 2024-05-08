8df147889e5649fcd23628c2ea63f5c223110dda
Adam Allport <adam@allport.me.uk>
2024-05-08 19:34:48 +0100

    it work

---
+++ build.bash
@@ -0,0 +1,2 @@
+rosdep install --from-path src --ignore-src -y
+colcon build --symlink-install
--- src/rosbot1/launch/lidar.launch.py
+++ src/rosbot1/launch/lidar.launch.py
@@ -1,21 +1,46 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node, LifecycleNode
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='ldlidar',
-            executable='ldlidar',
-            name='ldlidar',
-            output='screen',
-            parameters=[
-                {'serial_port': "/dev/serial1")},
-                {'topic_name': "scan"},
-                {'lidar_frame': "lidar_frame"},
-                {'range_threshold': "0.005"}
-            ]
-        )
-    ])
+    # Lifecycle manager configuration file
+    lc_mgr_config_path = os.path.join(
+        get_package_share_directory("ldlidar_node"), "params", "lifecycle_mgr.yaml"
+    )
+
+    lc_mgr_node = Node(
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager",
+        output="screen",
+        parameters=[
+            # YAML files
+            lc_mgr_config_path  # Parameters
+        ],
+    )
+
+    lidar_config_path = os.path.join(
+        get_package_share_directory('ldlidar_node'),
+        'params',
+        'ldlidar.yaml'
+    )
+
+    ldlidar_node = LifecycleNode(
+        package="ldlidar_node",
+        executable="ldlidar_node",
+        name="ldlidar_node",
+        namespace="",
+        output="screen",
+        parameters=[
+            # YAML files
+            lidar_config_path  # Parameters
+        ],
+    )
+
+    return LaunchDescription([lc_mgr_node, ldlidar_node])
