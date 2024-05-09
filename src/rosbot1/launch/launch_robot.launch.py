be88a32b25aeba4a7d5a3712789eec1434e2de59
Adam Allport <adam@allport.me.uk>
2024-05-09 17:09:55 +0100

    fix: Lidar

--- src/rosbot1/launch/launch_robot.launch.py
+++ src/rosbot1/launch/launch_robot.launch.py
@@ -13,24 +13,35 @@ from launch.event_handlers import OnProcessStart
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
-    package_name='rosbot1'
-    
+    package_name = "rosbot1"
+
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name), "launch", "rsp.launch.py"
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "false", "use_ros2_control": "true"}.items(),
     )
-    
+
     lidar = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','lidar.launch.py'
-                )])
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "lidar.launch.py",
+                )
+            ]
+        )
     )
-    
-    return LaunchDescription([
-        rsp,
-        lidar,
-    ])
-    
+
+    return LaunchDescription(
+        [
+            rsp,
+            lidar,
+        ]
+    )
